7eac3eb38f9347d17044566aa71f5d08a214eaa8
Alyssa Thompson <alyssaJT876@gmail.com>
2021-03-03 22:40:46 -0500

    Completed Lab 5

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,9 @@ from flask_sqlalchemy import SQLAlchemy
 from .config import Config
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "change this to be a more random key"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://lab5:Password123@localhost/lab5"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True 
 
 db = SQLAlchemy(app)
 
